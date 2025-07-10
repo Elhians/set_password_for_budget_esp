2fbc518bfb29b12bd28d79cc84069df66009675c
Elhians <elhianne04@gmail.com>
2025-07-10 15:20:27 +0000

    change of back-end link

--- src/services/httpClient.jsx
+++ src/services/httpClient.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://api-gestion-budget-esp.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
